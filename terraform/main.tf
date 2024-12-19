3b826324e41d0b147194f5f43e9ddb36556050e5
DmytroMDobrovolsky <144679745+DmytroMDobrovolsky@users.noreply.github.com>
2024-12-19 14:23:11 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,23 +6,25 @@ terraform {
       version = "~> 5.0"
     }
   }
-backend "s3" {
-    bucket = "lab67-my-tf-state"
-    key = "terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "lab67-my-tf-lockid" 
 
+  backend "s3" {
+    bucket         = "lab67-my-tf-state"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "lab67-my-tf-lockid"
   }
 }
 
 # Configure the AWS provider
 provider "aws" {
-  region     = "us-east-1"
+  region = "us-east-1"
 }
 
+# Create a Security Group
 resource "aws_security_group" "web_app" {
   name        = "web_app"
-  description = "security group"
+  description = "Security group for web application"
+
   ingress {
     from_port   = 80
     to_port     = 80
@@ -30,12 +32,13 @@ resource "aws_security_group" "web_app" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
- ingress {
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 65535
@@ -43,15 +46,16 @@ resource "aws_security_group" "web_app" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags= {
+  tags = {
     Name = "web_app"
   }
 }
 
+# Create an EC2 instance
 resource "aws_instance" "web_instance" {
   ami           = "ami-0866a3c8686eaeeba"
   instance_type = "t3.micro"
-  security_groups = ["web_app"]
+  vpc_security_group_ids = [aws_security_group.web_app.id] # Виправлено
 
   user_data = <<-EOF
   #!/bin/bash
@@ -60,18 +64,16 @@ resource "aws_instance" "web_instance" {
   sudo groupadd docker
   sudo usermod -aG docker $USER
   newgrp docker
-  docker pull  dmytromdobrovolsky/aws:latest
+  docker pull dmytromdobrovolsky/aws:latest
   docker run -it dmytromdobrovolsky/aws:latest
-
   EOF
-
 
   tags = {
     Name = "webapp_instance"
   }
 }
 
-
+# Output the public IP of the instance
 output "instance_public_ip" {
   value     = aws_instance.web_instance.public_ip
   sensitive = true
